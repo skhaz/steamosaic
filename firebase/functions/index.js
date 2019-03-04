eae8f825a09089f87ed01808778235011207c0f7
Rodrigo Delduca <rodrigodelduca@gmail.com>
2019-03-04 11:50:06 -0300

    Add ticker function in order to collect some stats

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,10 +1,12 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const { PubSub } = require('@google-cloud/pubsub');
+const { BigQuery } = require('@google-cloud/bigquery');
 
 admin.initializeApp();
 const firestore = admin.firestore();
 const pubsub = new PubSub();
+const bigquery = new BigQuery();
 
 
 exports.notify = functions.firestore
@@ -15,4 +17,36 @@ exports.notify = functions.firestore
     const topic = pubsub.topic(functions.config().pubsub.topic);
 
     return topic.publish(dataBuffer);
+  });
+
+exports.ticker = functions.pubsub
+  .topic('ticker')
+  .onPublish(async (_) => {
+    const query = `SELECT COUNT(*) FROM (
+      SELECT
+        REGEXP_EXTRACT(textPayload, r'user (\\w+)') AS steam_id
+      FROM
+        \`steamosaic.steam_ids.cloudfunctions_googleapis_com_cloud_functions_*\`
+      WHERE
+        textPayload LIKE '%fetching steam_id%'
+      GROUP BY
+        steam_id)`
+
+    const response = await bigquery.query(query);
+    const counter = response[0][0].f0_;
+    await firestore.doc('stats/users').set({ counter });
+
+    const result = await firestore
+      .collection('users')
+      .where('error', '>', '')
+      .limit(500)
+      .get();
+
+    const batch = firestore.batch();
+
+    result.forEach(doc => {
+      batch.delete(doc.ref);
+    });
+
+    await batch.commit();
   });
