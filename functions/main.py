46ee6f3624f57f2175301ad1f80abe99bd5a3a76
Rodrigo Delduca <rodrigodelduca@gmail.com>
2019-10-28 18:06:36 -0300

    cosmetic changes

--- functions/main.py
+++ functions/main.py
@@ -45,8 +45,8 @@ def get_steam_id(uid):
   return response.json()['response']['steamid']
 
 
-def get_games(steam_id):
-  response = session.get(GAMES_URL.format(steam_id))
+def get_games(sid):
+  response = session.get(GAMES_URL.format(sid))
   response.raise_for_status()
   result = response.json()['response']['games']
   return sorted(result, key=lambda g: g['playtime_forever'], reverse=True)
@@ -75,33 +75,32 @@ def pubsub(event, context):
   message = json.loads(
     base64.b64decode(event['data']).decode('utf-8'))
   uid = message['uid']
-  dref = db.collection('users').document(uid)
+  ref = db.collection('users').document(uid)
 
   try:
-    steam_id = str(get_steam_id(uid))
-    maximum = 10000
-    games = [n for n in get_games(steam_id) if n['img_logo_url']][:maximum]
+    sid = str(get_steam_id(uid))
+    maximum = 8192
+    games = [n for n in get_games(sid) if n['img_logo_url']][:maximum]
     build_url = lambda entry: MEDIA_URL.format(
       entry['appid'], entry['img_logo_url'])
     fetch = lambda game: functools.reduce(lambda g, f: f(g), [build_url, download], game)
     arr = np.array([g for g in map(fetch, games) if g is not None])
   except (KeyError, requests.exceptions.HTTPError):
-    dref.set({'error': "private profile or not found."})
+    ref.set({'error': 'private profile or not found.'})
     return
 
-  buffer = io.BytesIO()
-  columns = 10
-  nearest = lambda x, n: x - (x % n)
-  limit = nearest(len(arr), columns)
   try:
+    buffer = io.BytesIO()
+    columns = 10
+    nearest = lambda x, n: x - (x % n)
+    limit = nearest(len(arr), columns)
     generate(arr[:limit], columns).save(buffer, 'JPEG', quality=90)
   except ValueError:
-    dref.set({'error': "internal error or insufficient amount of games to generate the image."})
+    ref.set({'error': 'internal error or insufficient amount of games to generate the image.'})
     return
 
-  filename = os.path.join(steam_id[-1:], ''.join([steam_id, '.jpg']))
-  blob = bucket.blob(filename)
+  filepath = f'{sid[-1:]}/{sid}.jpg'
+  blob = bucket.blob(filepath)
   blob.upload_from_string(buffer.getvalue(), content_type='image/jpeg')
   blob.make_public()
-  url = 'https://%s/%s' % (os.environ['BUCKET'], filename)
-  dref.set({'url': url})
+  ref.set({'url': 'https://gcs.steamosaic.com/%s' % (filepath)})
