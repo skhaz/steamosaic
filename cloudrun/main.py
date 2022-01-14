21348adf9ee78c269de30bf09c2986b64cff5ed8
Rodrigo Delduca <rodrigodelduca@gmail.com>
2022-01-14 13:38:23 -0300

    Upgrade libraries and style fix

--- cloudrun/main.py
+++ cloudrun/main.py
@@ -1,111 +1,117 @@
-import os
+import base64
+import functools
 import io
 import json
-import tempfile
-import base64
 import logging
-import functools
-from urllib.parse import urlparse
+import os
+import tempfile
+import http
 
 import numpy as np
-import requests
-
-from PIL import Image
-from joblib import Memory
+from requests import Session
 from flask import Flask, request
-
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-from google.cloud import storage
-
-PROFILE_URL = os.environ['PROFILE_URL']
-GAMES_URL = os.environ['GAMES_URL']
-MEDIA_URL = os.environ['MEDIA_URL']
-
-# credentials.ApplicationDefault(), {'projectId': os.environ['GCP_PROJECT'],}
-firebase_admin.initialize_app()
+from google.cloud.firestore import Client as FirestoreClient
+from google.cloud.storage import Client as StorageClient
+from joblib import Memory
+from PIL import Image
 
 app = Flask(__name__)
 
-db = firebase_admin.firestore.client()
+firestore = FirestoreClient()
+
 memory = Memory(tempfile.gettempdir(), verbose=0)
-session = requests.Session()
-storage_client = storage.Client()
-bucket = storage_client.get_bucket(os.environ['BUCKET'])
+
+requests = Session()
+
+storage = StorageClient()
+
+bucket = storage.get_bucket(os.environ["BUCKET"])
+
+PROFILE_URL = os.environ["PROFILE_URL"]
+GAMES_URL = os.environ["GAMES_URL"]
+MEDIA_URL = os.environ["MEDIA_URL"]
+
+NO_CONTENT = "", http.HTTPStatus.NO_CONTENT
 
 
 def get_steam_id(uid):
-  logging.info(f'fetching steam_id of the user {uid}')
-  try:
-    return int(uid)
-  except ValueError:
-    pass
-  response = session.get(PROFILE_URL.format(uid))
-  response.raise_for_status()
-  return response.json()['response']['steamid']
+    logging.info(f"fetching steam_id of the user {uid}")
+    try:
+        return int(uid)
+    except ValueError:
+        pass
+    response = requests.get(PROFILE_URL.format(uid))
+    response.raise_for_status()
+    return response.json()["response"]["steamid"]
 
 
 def get_games(sid):
-  response = session.get(GAMES_URL.format(sid))
-  response.raise_for_status()
-  result = response.json()['response']['games']
-  return sorted(result, key=lambda g: g['playtime_forever'], reverse=True)
+    response = requests.get(GAMES_URL.format(sid))
+    response.raise_for_status()
+    result = response.json()["response"]["games"]
+    return sorted(result, key=lambda g: g["playtime_forever"], reverse=True)
 
 
 @memory.cache
 def download(url):
-  try:
-    image = Image.open(
-      io.BytesIO(session.get(url).content))
-    return np.asarray(image.convert('RGB'))
-  except (OSError, IOError):
-    logging.warning(f'failed to download or invalid image at {url}')
+    try:
+        image = Image.open(io.BytesIO(requests.get(url).content))
+        return np.asarray(image.convert("RGB"))
+    except (OSError, IOError):
+        logging.warning(f"failed to download or invalid image at {url}")
 
 
 def generate(array, columns=10):
-  length, height, width, intensity = array.shape
-  rows = length // columns
-  return Image.fromarray(
-    array.reshape(rows, columns, height, width, intensity)
-      .swapaxes(1, 2)
-      .reshape(height * rows, width * columns, intensity))
+    length, height, width, intensity = array.shape
+    rows = length // columns
+    return Image.fromarray(
+        array.reshape(rows, columns, height, width, intensity)
+        .swapaxes(1, 2)
+        .reshape(height * rows, width * columns, intensity)
+    )
 
 
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["POST"])
 def index():
-  message = request.get_json()['message']
+    message = request.get_json()["message"]
 
-  data = json.loads(
-    base64.b64decode(message['data']).decode('utf-8'))
-  uid = data['uid']
-  reference = db.collection('users').document(uid)
+    data = json.loads(base64.b64decode(message["data"]).decode("utf-8"))
+    uid = data["uid"]
+    reference = firestore.collection("users").document(uid)
 
-  try:
-    sid = str(get_steam_id(uid))
-    maximum = 9216
-    games = [n for n in get_games(sid) if n['img_logo_url']][:maximum]
-    build_url = lambda entry: MEDIA_URL.format(entry['appid'], entry['img_logo_url'])
-    fetch = lambda game: functools.reduce(lambda g, f: f(g), [build_url, download], game)
-    arr = np.array([g for g in map(fetch, games) if g is not None])
-  except (KeyError, requests.exceptions.HTTPError):
-    reference.set({'error': 'private profile or not found.'})
-    return ('', 204)
+    try:
+        sid = str(get_steam_id(uid))
+        maximum = 9216
+        games = [n for n in get_games(sid) if n["img_logo_url"]][:maximum]
+        build_url = lambda e: MEDIA_URL.format(e["appid"], e["img_logo_url"])
+        funcs = [build_url, download]
+        fetch = lambda game: functools.reduce(lambda g, f: f(g), funcs, game)
 
-  try:
-    buffer = io.BytesIO()
-    columns = 10
-    nearest = lambda x, n: x - (x % n)
-    limit = nearest(len(arr), columns)
-    generate(arr[:limit], columns).save(buffer, 'JPEG', quality=90)
-  except ValueError:
-    reference.set({'error': 'internal error or insufficient amount of games to generate the image.'})
-    return ('', 204)
+        array = np.array([g for g in map(fetch, games) if g is not None])
+    except (KeyError, requests.exceptions.HTTPError):
+        reference.set({"error": "private profile or not found."})
+        return NO_CONTENT
 
-  filepath = f'{sid[-1:]}/{sid}.jpg'
-  blob = bucket.blob(filepath)
-  blob.upload_from_string(buffer.getvalue(), content_type='image/jpeg')
-  blob.make_public()
-  reference.set({'url': 'https://gcs.steamosaic.com/%s' % (filepath)})
+    try:
+        buffer = io.BytesIO()
+        columns = 10
+        nearest = lambda x, n: x - (x % n)
+        limit = nearest(len(array), columns)
 
-  return ('', 204)
+        generate(array[:limit], columns).save(buffer, "JPEG", quality=90)
+    except ValueError:
+        reference.set(
+            {
+                "error": "internal error or insufficient amount of games to generate the image."
+            }
+        )
+
+        return NO_CONTENT
+
+    filepath = f"{sid[-1:]}/{sid}.jpg"
+    blob = bucket.blob(filepath)
+    blob.upload_from_string(buffer.getvalue(), content_type="image/jpeg")
+    blob.make_public()
+    reference.set({"url": "https://gcs.steamosaic.com/%s" % (filepath)})
+
+    return NO_CONTENT
